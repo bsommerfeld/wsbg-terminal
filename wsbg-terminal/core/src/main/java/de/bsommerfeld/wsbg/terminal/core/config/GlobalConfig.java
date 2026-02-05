a4d5fd79b2d4ce30c68fb4084fa33b1702a44b43
bsommerfeld <luziferisdoof@gmail.com>
2026-02-05 22:06:20 +0100

    chore: rename project to WSBG Terminal and remove Leviathan references

---
+++ wsbg-terminal/core/src/main/java/de/bsommerfeld/wsbg/terminal/core/config/GlobalConfig.java
@@ -0,0 +1,46 @@
+package de.bsommerfeld.wsbg.terminal.core.config;
+
+import de.bsommerfeld.jshepherd.core.ConfigurablePojo;
+import de.bsommerfeld.jshepherd.annotation.Comment;
+import de.bsommerfeld.jshepherd.annotation.Key;
+import de.bsommerfeld.jshepherd.annotation.Section;
+
+@Comment("WSBG Terminal - Global Configuration")
+public class GlobalConfig extends ConfigurablePojo<GlobalConfig> {
+
+    @Key("debug-mode")
+    @Comment("Enable detailed debug logging and UI inspection")
+    private boolean debugMode = false;
+
+    @Key("ui-reddit-visible")
+    @Comment("Visibility state of the Reddit List Panel")
+    private boolean redditListVisible = true;
+
+    @Section("market")
+    @Comment("Market Data Provider Settings")
+    private MarketConfig market = new MarketConfig();
+
+    @Section("agent")
+    @Comment("AI Agent Settings")
+    private AgentConfig agent = new AgentConfig();
+
+    public boolean isRedditListVisible() {
+        return redditListVisible;
+    }
+
+    public void setRedditListVisible(boolean redditListVisible) {
+        this.redditListVisible = redditListVisible;
+    }
+
+    public boolean isDebugMode() {
+        return debugMode;
+    }
+
+    public MarketConfig getMarket() {
+        return market;
+    }
+
+    public AgentConfig getAgent() {
+        return agent;
+    }
+}
